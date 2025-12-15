e71083f65d08262c42e710cb962f396b52f771e0
Guru <guru2228@gmail.com>
2025-12-14 18:29:24 -0600

    Add Zsh aliases and functions for development tools

---
+++ zsh/.config/zsh/aliases/shortcuts.zsh
@@ -0,0 +1,51 @@
+alias ls="ls --color"
+alias dev="cd ~/Projects"
+alias dsstore="find . -name '.DS_Store' -type f -delete"
+alias icloud="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs"
+
+# PHP Storm
+alias s.="phpstorm ."
+function phpstorm() {
+	open -na "PhpStorm.app" --args "$@"
+}
+
+# WebStorm
+alias w.="webstorm ."
+function webstorm() {
+	open -na "WebStorm.app" --args "$@"
+}
+
+# PyCharm
+alias c.="pycharm ."
+function pycharm() {
+	open -na "PyCharm Professional Edition.app" --args "$@"
+}
+
+# Python
+alias py="python"
+
+# Neovim
+alias n="nvim"
+
+# Tmuxifier
+alias tx="tmuxifier"
+
+# Debug ZSH startup time
+alias debugzsh="time zsh -i -c exit"
+
+# Hyperfine
+alias bench="hyperfine"
+
+# Darwin-Rebuild
+function drb() {
+  local nix_path=$XDG_CONFIG_HOME/nix
+  local dotfiles_path=$(readlink -f $XDG_CONFIG_HOME/zsh | xargs dirname | xargs dirname | xargs dirname )/nix
+  cp $dotfiles_path/flake.nix $nix_path
+  darwin-rebuild switch --flake $nix_path#macos
+}
+
+# MLX
+alias mlx="$HOME/Projects/ai/mlx/.venv/bin/mlx_lm.generate --ignore-chat-template -m 2048"
+
+# Color palette generation
+alias colors="$XDG_CONFIG_HOME/colors/generate_palette.sh"
